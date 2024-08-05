e4f357f44c743cee24da09194f9685f14556dfb3
bu-lo <chloe.boivin@outlook.com>
2024-08-05 16:14:51 +0200

    CSS done

--- src/main/java/fr/afpa/democss/App.java
+++ src/main/java/fr/afpa/democss/App.java
@@ -4,23 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-        // *** TITLE IN STAGE ***
-        primaryStage.setTitle(" EX AFPA CSS");
-
-        // ICON
-        Image icon = new Image(getClass().getResourceAsStream("afpa.png"));
-        primaryStage.getIcons().add(icon); // Add icon in stage
-
-        // *** SCENE ***
-
         Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setScene(scene);
